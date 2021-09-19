3e41a56f6bd245ae9824984409a5c2a82ba243f3
Mohsin <m.mohsin0395@gmail.com>
2021-09-19 22:02:27 +0530

    initioalize

---
+++ src/sept17FistDay/CompairTwoDates.java
@@ -0,0 +1,15 @@
+
+package sept17FistDay;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+public class CompairTwoDates {
+    public static void main(String[] args) throws ParseException{
+        SimpleDateFormat sdf=new SimpleDateFormat("dd/mm/yyyy");
+        Date date1=sdf.Parse("01/02/2016");
+        Date date1=sdf.Parse("10/02/2016");
+        if(date1.compairTo(date2));
+        
+    }
+    
+}
